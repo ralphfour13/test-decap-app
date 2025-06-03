a23a3b98e922b359ebb3f9371afb968688b8a069
ralph <ralph@four13.co>
2025-06-03 21:05:06 +0800

    initial commit

---
+++ content/blog/hello-world.md
@@ -0,0 +1,10 @@
+# content/blog/hello-world.md
+
+---
+
+title: "Hello World"
+date: "2025-06-03"
+
+---
+
+This is a test post written in Markdown.
---
+++ lib/posts.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "content/blog");
+
+export function getPostSlugs() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getPostBySlug(slug: string) {
+  const realSlug = slug.replace(/\.md$/, "");
+  const fullPath = path.join(postsDirectory, `${realSlug}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  return {
+    slug: realSlug,
+    meta: data,
+    content,
+  };
+}
