db83a4422d418da59fe6a012a92770428f22a200
ralph <ralph@four13.co>
2025-07-22 09:27:14 +0800

    update pricing

--- src/page-list.tsx
+++ src/page-list.tsx
@@ -1,5 +1,5 @@
 export default async function PageList() {
-  const response = await fetch("http://localhost:3000/api/pages");
+  const response = await fetch("https://test-decap-app.netlify.app/api");
   const pages = await response.json();
 
   return pages;
